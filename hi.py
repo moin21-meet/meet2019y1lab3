dbcdebc03890b6861c66892606f4d406abcb1f8a
moin21-meet <moin21@meet.mit.edu>
2019-07-23 07:19:52 -0400

    Stopping here

---
+++ hi.py
@@ -0,0 +1,8 @@
+'multiply' * 3
+x = input('what ur name? ')
+#print('hello there,', x[0].upper() + x[1:])
+print(x.capitalize())
+print('ur name ia', len(x))
+print(x[0].upper(), x[-1].upper())
+print(x[1:-1])
+print(" hi ted, 'hi moin' ")
--- lab_3.py
+++ lab_3.py
@@ -1,14 +1,5 @@
 n = int(input())
-answer=''    
-for i in range(0,n):
-    i=i+1 
-    i=str(i)
-    answer=answer+i
-print(answer)
-
-
-
-a=int(input())
-for i in range(0,n):
-    i+=1
+i=0
+while i < n:
+    i +=1
     print(i)
