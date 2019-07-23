20e77d93221d4ed30c5b39f9b107e96796ee5255
moin21-meet <moin21@meet.mit.edu>
2019-07-23 05:30:14 -0400

    Stopping here

---
+++ bonus3.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+print(a)
+print(b)
+if a>b: 
+    print(a-b)
+else:
+    print(b-a)    
+abs(a)
---
+++ lab_3.py
@@ -0,0 +1,14 @@
+n = int(input())
+answer=''    
+for i in range(0,n):
+    i=i+1 
+    i=str(i)
+    answer=answer+i
+print(answer)
+
+
+
+a=int(input())
+for i in range(0,n):
+    i+=1
+    print(i)
